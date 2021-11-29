50b4945224c2482509e95cdc38c6f982ae1dbe6c
Temirlannn <93139917+Temirlannn@users.noreply.github.com>
2021-11-29 18:59:52 +0600

    add

--- app/src/main/java/com/itacademy/newprojec/MainActivity2.kt
+++ app/src/main/java/com/itacademy/newprojec/MainActivity2.kt
@@ -2,15 +2,12 @@ package com.itacademy.newprojec
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity2 : AppCompatActivity() {
 
     lateinit var imageView: ImageView
-    lateinit var imageButton: Button
+    lateinit var imageButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
